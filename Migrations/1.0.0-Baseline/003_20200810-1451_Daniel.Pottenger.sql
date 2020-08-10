f6b976a3c1a21934fe7b0565cd569470636ad5ba
Daniel Pottenger <gamerpotty@gmail.com>
2020-08-10 14:51:13 +0100

    Tabletable

---
+++ Migrations/1.0.0-Baseline/003_20200810-1451_Daniel.Pottenger.sql
@@ -0,0 +1,10 @@
+﻿-- <Migration ID="72b07c00-c00a-4162-9bb9-87dfa5af2543" />
+GO
+
+PRINT N'Creating [dbo].[TableTable]'
+GO
+CREATE TABLE [dbo].[TableTable]
+(
+[ColumnOne] [nchar] (10) NULL
+)
+GO
---
+++ Schema-Model/dbo/Tables/TableTable.sql
@@ -0,0 +1,5 @@
+CREATE TABLE [dbo].[TableTable]
+(
+[ColumnOne] [nchar] (10) NULL
+)
+GO
