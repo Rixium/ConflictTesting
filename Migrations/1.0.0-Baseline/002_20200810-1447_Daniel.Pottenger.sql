9ebe5b492d6ed11cc2dc7b0c87a1135f5a82d6fc
Daniel Pottenger <gamerpotty@gmail.com>
2020-08-10 14:48:07 +0100

    Add some other table

---
+++ Migrations/1.0.0-Baseline/002_20200810-1447_Daniel.Pottenger.sql
@@ -0,0 +1,10 @@
+﻿-- <Migration ID="4b2d7e34-e9d6-49e0-92fa-16a3b5f8db31" />
+GO
+
+PRINT N'Creating [dbo].[SomeOtherTable]'
+GO
+CREATE TABLE [dbo].[SomeOtherTable]
+(
+[SomeOtherTable] [nchar] (10) NULL
+)
+GO
---
+++ Schema-Model/dbo/Tables/SomeOtherTable.sql
@@ -0,0 +1,5 @@
+CREATE TABLE [dbo].[SomeOtherTable]
+(
+[SomeOtherTable] [nchar] (10) NULL
+)
+GO
